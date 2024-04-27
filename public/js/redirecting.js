2185eb36be4c1e92c1a49f7ab85c7f013813dcc4
DocTheDivinity <darkos879@gmail.com>
2024-04-27 18:19:34 +0200

    Fixed routing and visual issues with login and signup

---
+++ public/js/redirecting.js
@@ -0,0 +1,10 @@
+function redirectToLoginPage() {
+    window.location.href = "/login"; // Redirect to the login route
+}
+
+function redirectToSignUpPage() {
+    window.location.href = "/signup"; // Redirect to the login route
+}
+
+module.exports = redirectToLoginPage();
+module.exports = redirectToSignUpPage();
