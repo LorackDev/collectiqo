8671ffa11f11d8055285f5b1d3a6190ae734140a
Lorenz Bindernagel <lorenz.bindernagel@icloud.com>
2024-11-11 22:52:30 +0100

    Add HTTPS support with SSL key and certificate (we need to set this up for all you locally)
    Integrated HTTPS by reading SSL key and certificate files from environment-specified paths. Updated server to listen on port 3000 over HTTPS, enhancing security for the application. Includes utility function to resolve home directory paths.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const session = require('express-session');
 const dotenv = require("dotenv");
 const path = require("path");
+const os = require("os");
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const port = process.env.PORT;
 const app = express();
 
 const routeLoader = require('./utils/routeLoader');
+
+function resolveHome(filepath) {
+    if (filepath[0] === '~') {
+        return path.join(os.homedir(), filepath.slice(1));
+    }
+    return filepath;
+}
+
+const options = {
+    key: fs.readFileSync(resolveHome(process.env.SSL_KEY_PATH)),
+    cert: fs.readFileSync(resolveHome(process.env.SSL_CERT_PATH))
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,10 +42,8 @@ app.get("/", async function(req, res) {
     res.render("index.ejs");
 });
 
-
-
 routeLoader(app);
 
-app.listen(port, () => {
-    console.log(`Server Started on port ${port}...`);
+https.createServer(options, app).listen(3000, () => {
+    console.log('App running on https://dev.collectiqo.com:3000');
 });
