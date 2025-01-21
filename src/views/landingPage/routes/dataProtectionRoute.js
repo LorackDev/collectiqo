5ce696810f4f252fd727f5477d09fc4b2c27516c
DocTheDivinity <darkos879@gmail.com>
2025-01-21 15:29:00 +0100

    I did an oopsie

--- src/views/landingPage/routes/dataProtectionRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/dataProtection', (req, res) => {
-    res.render('landingPage/pages/dataProtection.ejs');
-});
-
-module.exports = router;
